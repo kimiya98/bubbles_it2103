4d928f6ed0765573235ec62df01cd51112b57472
kimiya98 <kittyap12@gmail.com>
2016-09-27 13:57:51 +0800

    added a button lmao xd

--- home.php
+++ home.php
@@ -3,5 +3,6 @@
 		<link rel='stylesheet' href='css/bootstrap.min.css'>
 	</head>
 	<body>
+		<button class='btn btn-danger'>!PRESS THIS TO GET A SMALL LOAN OF A MILLION DOLLARS!</button>
 	</body>
 	</html>
